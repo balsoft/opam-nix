833805199016b799858be30781181bbed7d1d8ac
Roman Melnikov <roman.melnikov@serokell.io>
2021-07-01 14:13:54 +0300

    fixup! Don't provide same deps twice

--- opam-nix.hs
+++ opam-nix.hs
@@ -36,8 +36,7 @@ data PackageInputs = PackageInputs
 printInputs :: PackageInputs -> String
 printInputs PackageInputs{..} =
   "{ stdenv, fetchurl, lib"
-  <> pPrintInputs piNativeBuildInputs
-  <> pPrintInputs piBuildInputs
+  pPrintInputs (union piNativeBuildInputs piBuildInputs))
   <> pPrintInputs (Set.map (<>" ? null") onlyCheckInputs)
   <> ", extraArgs ? { } }@args:\n"
   where
